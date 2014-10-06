9d0224da53683083dacaf4f38cb2e286f7f70f95
bmonsalves <bdo.monsalves@gmail.com>
2014-10-06 20:56:37 -0300

    modularizacion

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ make test
 
 #ejecucion
 node server
+
+#para no reiniciar el server al hacer cambios
+npm install -g nodemon
+nodemon server
+
+#puerto
+3000
+--------------------------
+
+#POSTMAN
+
+#POST nota de prueba
+http://localhost:3000/notas
+{
+	"nota": {
+	  "title": "nota 1",
+	  "description": "prueba de nota 1",
+	  "type": "js",
+	  "body": "cuerpo json nota"
+	}
+}
+
+#GET 
+http://localhost:3000/notas/<id_nota>
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
  * Locals
  */
 var server = module.exports = express();
-var db = {};
+
 
 /*
 	MIDDLEWARE
@@ -20,39 +20,8 @@ server.use(bodyParser.json({ type: 'application/json' }));
 /*
 	ROUTES
 */
-server.post('/notas',function(req, res){
-
-	//obtiene el json
-	console.log("POST", req.body);
-	//console.log(req.is('json'));
-
-	// manipulate request
-	var notaNueva = req.body.nota;
-	notaNueva.id = Date.now();
-
-	db[notaNueva.id] = notaNueva;
-
-	// prepare response
-	res.status(201);
-
-	// send response
-	res.send({
-	nota: notaNueva
-	});
-});
-
-//parametro opcional :id? 
-server.get('/notas/:id?', function(req,res){
-	console.log('GET /notas/%s',req.params.id);
-
-	var id = req.params.id;
-	var nota = db[id];
-
-	res.json({
-		notas: nota
-	});
-});
-
+var notas = require('./lib/notas');
+server.use(notas);
 
 /*
 START SERVER
